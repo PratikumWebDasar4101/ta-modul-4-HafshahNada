83c309a9a9100d97d84c0e0e9397a81eb6bef5ab
HafshahNada <42725368+HafshahNada@users.noreply.github.com>
2018-09-26 22:06:05 +0700

    Update proses_login.php

--- proses_login.php
+++ proses_login.php
@@ -1,3 +1,18 @@
-<?php
+<?php 
+session_start();
 
+$user = array(
+	"nama" => "Nama",
+	"username" => "admin",
+	"password" => 1357,
+	"notelp" => "081773388764"
+);
+
+if ($user["username"] == $_POST["username"] && $user["password"] == $_POST["password"]) {
+	$_SESSION["username"] = $user["username"];
+	$_SESSION["user"] = $user;
+	header("Location: formawal.php");
+}else {
+	header("Location: login.php");
+}
 ?>
